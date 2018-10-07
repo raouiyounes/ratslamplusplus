8b3595ee6cfb31e3d10ae5a837f60697b548220f
Younès <younes.fsr@gmail.com>
2018-10-07 13:31:10 +0100

    work in progress on SASTSlam class ...

--- ratslam/FASTSlam.py
+++ ratslam/FASTSlam.py
@@ -9,72 +9,96 @@ class packet:
 
 class Map:
 	def __init__(self):
-		self.muX=[]
-		self.muY=[]
-		self.muZ=[]
+		self.muX=0
+		self.muY=0
+		self.muZ=0
+		self.vObject=Vertex()
 class FASTSlam:
 	
-	def __init__(self):
+	def __init__(self,groundTruth):
 		
 		pk=packet()
-			
+		self.truePose=groundTruth	
 		# all the poses of the robot as obtained from pcn
 		self.packetS=[]
 		self.mapR=[]
 		self.observation=[]
+		self.Rt=0.2*np.eye(2,2)
+		self.EXP_INITIAL_EM_DEG=90.0
+        self.accum_delta_facing=self.EXP_INITIAL_EM_DEG*math.pi/180
+		self.accum_delta_x=0
+		self.accum_delta_y=0
 		
 	# receive packet from can and place it in a list
 	def setPacket(self,packetCAN):
-		
 		pk=packet()
 		pk.x=packetCAN.x
 		pk.y=packetCAN.y
 		pk.th=packetCAN.th
 		self.packets.append(pk)
-	
-	
 	# compute the 3D coordinate of an object	
 	def t3DObject(self,objectR):
 		
-		
-		
-	
 	# data association
-	def dataAssociation(self,objects,packetCAN):
+	def mapping(self,objectR,packetCAN,vtrans,vrot,time_diff_s):
+		
+		# computation of the true positions
+		vtrans=vtrans*time_diff_s
+        vrot*=time_diff_s
+        self.accum_delta_facing=self.clip_rad_180(self.accum_delta_facing+vrot)
+        self.accum_delta_x=self.accum_delta_x+vtrans*math.cos(self.accum_delta_facing)
+        self.accum_delta_y=+vtrans*math.sin(self.accum_delta_facing)
+        self.accum_delta_time_s+=time_diff_s
+ #tracking of features
+ 
+ # ading a loop that interates on all the objects of a precise view
 		numberOflandmarks=0
-		for i in len(mapR):
-			
-			landmark2Object=landm2Feat(mapR[i])
+		for i in len(self.mapR):
+			#data association
+			landmark2Object=landm2Feat(self.mapR[i])
+			#active search
 			if objectR.cv== landmark2Object.cv and objectR.lv==landmark2Object.lv and match(objectR.indexOfView,landmark2Object.indexOfView)==1:
 				distPoseLand=np.zeros(len(packetCAN.x))
 				direcPoseLand=np.zeros(len(packetCAN.x))
+				
+				# Update Kalman Filters
 				for j in len(packetCAN):
-					
-					
 					Mu=[mapR[i].x,mapR[i].y]
-					
 					distPoseLand[j]=sqrt((packetCAN.x[j]-Mu[0])**2+(packetCAN.y[j]-Mu[1])**2)
 					direcPoseLand[j]=math.atan((packetCAN.y[j]-Mu[1])/(packetCAN.x[j]-Mu[0]))
-					if (self.isRobPerceLand(distPoseLand[j],direcPoseLand[j])==True):
-						self.ComputeJacobG([packetCAN.x[j],packetCAN.y[j]],Mu)
-						numberOflandmarks+=1
-	
-		if numberOflandmarks!=len(mapR)-1:
-			
-			
-			
-			
-			
-		for i in len(poseMap):
-			for j in len(poseMap[0]):
 				
-				of poseMap[i][j]==True:
+					TruedistPoseLand[j]=sqrt((self.accum_delta_x-Mu[0])**2+(self.accum_delta_y-Mu[1])**2)
+					TruedirecPoseLand[j]=math.atan((self.accum_delta_y-Mu[1])/(self.accum_delta_x-Mu[0]))
+				
+					G=self.ComputeJacobG([packetCAN.x[j],packetCAN.y[j]],Mu,distPoseLand[j])
+					Qn=np.transpose(G)*Cov*G+self.Rt
 					
+					zt_pred=[distPoseLand[j] direcPoseLand[j]]
+					zt_measure=[TruedistPoseLand[j],TruedirecPoseLand[j]]
+					wn=sqrt(np.abs(2*math.pi*Qn))**-1*np.exp(-0.5*(
+					K=Cov*G*np.linalg.inv(Q)
+					Cov=(np.eye(3,3)-K*G)*Cov
+					self.updateMeasCova()
+			
+			else:
+				numberOflandmarks+=1
+	
+		if numberOflandmarks==len(mapR)-1:
+			Mu=self.feat3land(objects)
+			map_i=Map()
+			map_i.muX=Mu[0]
+			map_i.muY=Mu[1]
+			map_i.muZ=Mu[2]
+			map_i.vObject=objectR
+			self.mapR.append(map_i)
+			
+			
+			
 				
 				
-	 def ComputeJacobG(self,poseOfPartic,poseOflandM):
+	 def ComputeJacobG(self,poseOfPartic,poseOflandM,r):
 		 
-		 return [[poseOfPartic[0]-poseOflandM[0], poseOfPartic[1]-poseOflandM[1],0],[poseOflandM[1]-poseOfPartic[1], poseOfPartic[0]-poseOflandM[0],-1]]
+		 return [[poseOfPartic[0]-poseOflandM[0], poseOfPartic[1]-poseOflandM[1],0],[(poseOflandM[1]-poseOfPartic[1])/r, (poseOfPartic[0]-poseOflandM[0])/r,-1]]
 		 
 				
 				
