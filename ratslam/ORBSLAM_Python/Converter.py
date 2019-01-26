a9f350954fcabd7fb79be32b507362bfd63ce49b
Younès <younes.fsr@gmail.com>
2019-01-26 13:19:55 +0100

    adding the class Frame and converter

---
+++ ratslam/ORBSLAM_Python/Converter.py
@@ -0,0 +1,6 @@
+class Converter:
+    def toDescriptorVector(self,Descriptors):
+        vDesc=[]
+        for i in Descriptors
+            vDesc.append(Descriptors[i])
+        return  vDesc
--- ratslam/ORBSLAM_Python/Frame.py
+++ ratslam/ORBSLAM_Python/Frame.py
@@ -8,7 +8,19 @@ Created on 2 janv. 2019
 import numpy as np
 import cv2
 import MapPoint
+import Converter as convt
+import sys
+
+sys.path.append('/home/younes/PycharmProjects/RatSLAM++1.0/CSAIL_Lim/src/ratslam_python/src/ratslam')
+import DBoWpy.TemplateVocabulary as mpORBvocabulary
+
+import DBoWpy.BoWvector
+
+import
 from cv_bridge import CvBridge, CvBridgeError
+
+FRAME_GRID_ROWS=48
+FRAME_GRID_COLS=64
 
 class Frame(object):
     '''
@@ -29,6 +41,7 @@ class Frame(object):
         self.mbInitialComputations=True
         self.mDistCoef=[]
         self.mvScaleFactors=mvScaleFactors
+        mBowVec=
 
         orb = cv2.ORB_create(nfeatures=int(.25 * (bd.shape[0] * bd.shape[1])),
                              edgeThreshold=patch_size,
@@ -62,20 +75,49 @@ class Frame(object):
 
         # This is done for the first created Frame
         mnId = Frame.nNextId+1;
-        mfScaleFactor=orb.getScaleFactor()
-        mnScaleLevels=orb.getNLevels()
+        self.mfScaleFactor=orb.getScaleFactor()
+        self.mnScaleLevels=orb.getNLevels()
 
-        mvScaleFactors=np.zeros(mnScaleLevels)
-        mvLevelSigma2=np.zeros(mnScaleLevels)
-        mvScaleFactors[0] = 1.0
-        mvLevelSigma2[0] = 1.0
-        for i in range(1,mnScaleLevels):
-            mvScaleFactors[i] = mvScaleFactors[i - 1] * mfScaleFactor;
-            mvLevelSigma2[i] = mvScaleFactors[i] * mvScaleFactors[i];
+        self.mvScaleFactors=np.zeros(self.mnScaleLevels)
+        self.mvLevelSigma2=np.zeros(self.mnScaleLevels)
+        self.mvScaleFactors[0] = 1.0
+        self.mvLevelSigma2[0] = 1.0
+        for i in range(1,self.mnScaleLevels):
+            self.mvScaleFactors[i] = self.mvScaleFactors[i - 1] * self.mfScaleFactor;
+            self.mvLevelSigma2[i] = self.mvScaleFactors[i] * self.mvScaleFactors[i];
+
+        self.mvInvLevelSigma2=np.zeros(len(self.mvLevelSigma2))
+        for i in range(self.mnScaleLevels):
+            self.mvInvLevelSigma2[i] =1/self.mvLevelSigma2[i];
+
+        self.mvInvLevelSigma2.reshape(len(self.mvLevelSigma2))
+        for i in range(self.mnScaleLevels):
+            self.mvInvLevelSigma2[i]=1/self.mvInvLevelSigma2[i]
+
+        #Assign Features to Grid Cells
+        nReserve = 0.5 * N/(FRAME_GRID_COLS*FRAME_GRID_ROWS)
+        for i in range(FRAME_GRID_COLS):
+            for j in range(FRAME_GRID_ROWS):
+                mGrid=[[[0]*nReserve]*FRAME_GRID_ROWS]*FRAME_GRID_COLS
+
+
+        for i in range(len(self.mvKeysUn)):
+            kp=self.mvKeysUn[i]
+            nGridPosX=0, nGridPosY=0;
+            if self.PosInGrid(kp,nGridPosX,nGridPosY):
+                mGrid[nGridPosX][nGridPosY].append(i)
+
+
+        # TODO -->  mvbOutlier = vector < bool > (N, false);
 
         # computation of bag of words
     def ComputeBoW(self):
         self.BOW.add(self.mDescriptors)
+
+        if not self.mBowVec:
+            vCurrentDesc=convt.toDescriptorVector(self.mDescriptors)
+            mpORBvocabulary.transform(vCurrentDesc,mBowVec,mFeatVec,4);
+
         
     def UndistortKeyPoints(self):
         if self.mDistCoef[0]==0.0:
@@ -105,3 +147,17 @@ class Frame(object):
         
         # Initiate ORB de
         
+
+
+
+    def PosInGrid(kp,posX,posY):
+        posX = np.round((kp.pt.x-Frame.mnMinX)*self.mfGridElementWidthInv)
+        posY = np.round((kp.pt.y-Frame.mnMinY)*self.mfGridElementHeightInv)
+        #    //Keypoint's coordinates are undistorted, which could cause to go out of the image
+        if posX<0 or posX>=FRAME_GRID_COLS or posY <0 or posY>=FRAME_GRID_ROWS:
+            return False
+        return True
+
+
+
+
